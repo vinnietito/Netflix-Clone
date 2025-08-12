3b6149ca0765b833a2edb8acabd64a1b6673fff0
vinnietito <vincentnjuguna69@gmail.com>
2025-08-12 23:43:27 +0300

    Commits

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
 
+
+
 const TitleCards = () => {
+
+
+const cardsRef = useRef();
+
+const handleWheel = (event)=>{
+    event.preventDefault();
+    cardsRef.current.scrollLeft += event.deltaY;
+}
+
+useEffect(()=>{
+    cardsRef.current.addEventListener('wheel', handleWheel);
+}, [])
+
+
   return (
     <div className='titlecards'>
       <h2>Popular on Netflix</h2>
-      <div className="card-list">
+      <div className="card-list" ref={cardsRef}>
         {cards_data.map((card, index)=>{
             return <div className="card" key={index}>
                 <img src={card.image} alt="" />

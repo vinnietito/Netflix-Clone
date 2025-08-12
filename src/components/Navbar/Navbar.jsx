5ddb7944f41f94fd25c4dc6a7036efab3a67bcf0
vinnietito <vincentnjuguna69@gmail.com>
2025-08-12 16:45:18 +0300

    Commits

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,6 +3,10 @@ import './Navbar.css'
 import logo from '../../assets/logo.png'
 import search_icon from '../../assets/search_icon.svg'
 import bell_icon from '../../assets/bell_icon.svg'
+import profile_img from '../../assets/profile_img.png'
+import caret_icon from '../../assets/caret_icon.svg'
+
+
 
 
 const Navbar = () => {
@@ -23,6 +27,10 @@ const Navbar = () => {
             <img src={search_icon} alt="" className='icons' />
             <p>Children</p>
             <img src={bell_icon} alt="" className='icons'/>
+            <div className="navbar-profile">
+                <img src={profile_img} alt="" className='profile'/>
+                <img src={caret_icon} alt="" />
+            </div>
         </div>
       
     </div>

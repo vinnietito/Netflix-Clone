d30333413076637ffc904b2cc0f5a12ff833c6c3
vinnietito <vincentnjuguna69@gmail.com>
2025-08-12 18:17:12 +0300

    Commits

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,9 @@ import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
 import hero_banner from '../../assets/hero_banner.jpg'
 import hero_title from '../../assets/hero_title.png'
+import play_icon from '../../assets/play_icon.png'
+import info_icon from '../../assets/info_icon.png'
+
 
 const Home = () => {
   return (
@@ -15,6 +18,10 @@ const Home = () => {
             <p>Discovering his ties to a secret ancient order, a young 
                 man living in modern Instanbul embarks on a quest to save the 
                 city from an immortal enemy.</p>
+            <div className="hero-btns">
+                <button className='btn'><img src={play_icon} alt="" />Play</button>
+                <button className='btn dark-btn'><img src={info_icon} alt="" />More Info</button>
+            </div>
         </div>
       </div>
     </div>

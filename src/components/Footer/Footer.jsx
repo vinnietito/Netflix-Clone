379c9fbf11edff79a5762ef615753a3480970d53
vinnietito <vincentnjuguna69@gmail.com>
2025-08-12 23:59:50 +0300

    Commits

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import './Footer.css'
+import youtube_icon from '../../assets/youtube_icon.png'
+import twitter_icon from '../../assets/twitter_icon.png'
+import instagram_icon from '../../assets/instagram_icon.png'
+import facebbok_icon from '../../assets/facebook_icon.png'
+
 
 const Footer = () => {
   return (
     <div className='footer'>
-      
+      <div className="footer-icons">
+        <img src={youtube_icon} alt="" />
+        <img src={twitter_icon} alt="" />
+        <img src={instagram_icon} alt="" />
+        <img src={facebbok_icon} alt="" />
+      </div>
     </div>
   )
 }

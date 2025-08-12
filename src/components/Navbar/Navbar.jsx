c787902bce4a3b39a886cd8bcd0e546667480afc
vinnietito <vincentnjuguna69@gmail.com>
2025-08-12 16:34:22 +0300

    Commits

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './Navbar.css'
+import logo from '../../assets/logo.png'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
+        <div className="navbar-left">
+            <img src={logo} alt="" />
+            <ul>
+                <li>Home</li>
+                <li>TV Shows</li>
+                <li>Movies</li>
+                <li>Home</li>
+                <li>Home</li>
+            </ul>
+        </div>
+        <div className="navbar-right"></div>
       
     </div>
   )

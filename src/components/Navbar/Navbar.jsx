40ec1666ad57a644409a0b2c5eb9f06dbbc243b2
vinnietito <vincentnjuguna69@gmail.com>
2025-08-12 16:40:05 +0300

    Commits

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
+import search_icon from '../../assets/search_icon.svg'
+import bell_icon from '../../assets/bell_icon.svg'
+
 
 const Navbar = () => {
   return (
@@ -11,11 +14,16 @@ const Navbar = () => {
                 <li>Home</li>
                 <li>TV Shows</li>
                 <li>Movies</li>
-                <li>Home</li>
-                <li>Home</li>
+                <li>New & Popular</li>
+                <li>My List</li>
+                <li>Browse by Languages</li>
             </ul>
         </div>
-        <div className="navbar-right"></div>
+        <div className="navbar-right">
+            <img src={search_icon} alt="" className='icons' />
+            <p>Children</p>
+            <img src={bell_icon} alt="" className='icons'/>
+        </div>
       
     </div>
   )

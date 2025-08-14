6d5c399dd9d36810c74fd8717c9cd38bf5c11f36
vinnietito <vincentnjuguna69@gmail.com>
2025-08-14 17:23:27 +0300

    Commits

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -28,10 +28,10 @@ const Home = () => {
         </div>
       </div>
       <div className="more-cards">
-        <TitleCards title={"Blockbuster Movies"}/>
-        <TitleCards title={"Only on Netflix"}/>
-        <TitleCards title={"Upcoming"}/>
-        <TitleCards title={"Top pics for You"}/>
+        <TitleCards title={"Blockbuster Movies"} category={"top_rated"}/>
+        <TitleCards title={"Only on Netflix"} category={"popular"}/>
+        <TitleCards title={"Upcoming"} category={"upcoming"}/>
+        <TitleCards title={"Top pics for You"} category={"now_playing"}/>
       </div>
       <Footer/>
     </div>

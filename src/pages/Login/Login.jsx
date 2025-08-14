101c70182cae2b69452041babf55bd555da16b21
vinnietito <vincentnjuguna69@gmail.com>
2025-08-14 14:02:12 +0300

    Commits

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
           {signState==="Sign Up"?<input type="text" placeholder='Your name' />:<></>}
           <input type="email" placeholder='Email' />
           <input type="password" placeholder='Password'/>
-          <button>Sign Up</button>
+          <button>{signState}</button>
           <div className="form-help">
             <div className="remember">
               <input type="checkbox" />
@@ -26,8 +26,9 @@ const Login = () => {
           </div>
         </form>
         <div className="form-switch">
-          <p>New to Netflix? <span>Sign Up Now</span></p>
-          <p>Already have Account? <span>Sign In Now</span></p>
+          {signState==="Sign In"? <p>New to Netflix? <span onClick={()=>{setSignState("Sign Up")}}>Sign Up Now</span></p>:<p>Already have Account? <span onClick={()=>{setSignState("Sign In")}}>Sign In Now</span></p>}
+          
+          
         </div>
       </div>
     </div>

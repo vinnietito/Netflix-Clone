508f3b20a5d80bd2c79be59f8708ffc4aa2218f1
vinnietito <vincentnjuguna69@gmail.com>
2025-08-14 13:38:41 +0300

    Commits

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 import logo from '../../assets/logo.png'
 
 const Login = () => {
+
+  const [signState, setSignState] = useState("Sign In")
+
+
   return (
     <div className='login'>
       <img src={logo} className='login-logo' alt="" />
       <div className="login-form">
-        <h1>Sign Up</h1>
+        <h1>{signState}</h1>
         <form>
-          <input type="text" placeholder='Your name' />
+          {signState==="Sign Up"?<input type="text" placeholder='Your name' />:<></>}
           <input type="email" placeholder='Email' />
           <input type="password" placeholder='Password'/>
           <button>Sign Up</button>

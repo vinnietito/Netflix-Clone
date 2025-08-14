f5ce069e4558dbbd399bf83b598de70c7c910431
vinnietito <vincentnjuguna69@gmail.com>
2025-08-14 13:07:09 +0300

    Commits

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -8,7 +8,19 @@ const Login = () => {
       <img src={logo} className='login-logo' alt="" />
       <div className="login-form">
         <h1>Sign In</h1>
-        <form action=""></form>
+        <form>
+          <input type="text" placeholder='Your name' />
+          <input type="email" placeholder='Email' />
+          <input type="password" placeholder='Password'/>
+          <button>Sign In</button>
+          <div className="form-help">
+            <div className="remember">
+              <input type="checkbox" />
+              <label htmlFor="">Remember Me</label>
+            </div>
+            <p>Need Help?</p>
+          </div>
+        </form>
       </div>
     </div>
   )

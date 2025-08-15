abe34dcba63ae45b260135430ee8a81c9e3b571e
vinnietito <vincentnjuguna69@gmail.com>
2025-08-15 09:47:54 +0300

    Commits

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import './Player.css'
 import back_arrow_icon from '../../assets/back_arrow_icon.png'
+import { useParams } from 'react-router-dom'
 
 const Player = () => {
+
+  const {id} = useParams();
 
   const [apiData, setApiData] = useState({
     name: "",
@@ -21,7 +24,7 @@ const options = {
 };
 
 useEffect(()=>{
-  fetch('https://api.themoviedb.org/3/movie/755898/videos?language=en-US', options)
+  fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
   .then(res => res.json())
   .then(res => setApiData(res.results[0]))
   .catch(err => console.error(err));
@@ -34,9 +37,9 @@ useEffect(()=>{
       <img src={back_arrow_icon} alt="" />
       <iframe width='90%' height='90%' src={`https://www.youtube.com/embed/${apiData.key}`} title='trailer' frameBorder='0' allowFullScreen></iframe>
       <div className="player-info">
-        <p>apiData.published_at</p>
-        <p>apiData.name</p>
-        <p>apiData.</p>
+        <p>{apiData.published_at.slice(0, 10)}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.type}</p>
       </div>
     </div>
   )

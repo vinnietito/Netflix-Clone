d1cd409e76cbc56a701eb42e4f465222223fa270
vinnietito <vincentnjuguna69@gmail.com>
2025-08-16 18:50:29 +0300

    Commits

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -45,10 +45,10 @@ useEffect(()=>{
       <h2>{title?title:"Popular on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
         {apiData.map((card, index)=>{
-            return <link to={`/player/${card.id}`} className="card" key={index}>
+            return <Link to={`/player/${card.id}`} className="card" key={index}>
                 <img src={`https://image.tmdb.org/t/p/w500` +card.backdrop_path} alt="" />
                 <p>{card.original_title}</p>
-            </link>
+            </Link>
         })}
       </div>
     </div>

ed2aca56a5102c7f2dd1ea9cfcf171d5326e56c6
vinnietito <vincentnjuguna69@gmail.com>
2025-08-16 15:40:57 +0300

    Commits

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -41,7 +41,7 @@ useEffect(()=>{
 
 
   return (
-    <div className='titlecards'>
+    <div className=''>
       <h2>{title?title:"Popular on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
         {apiData.map((card, index)=>{

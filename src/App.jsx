f66d520e8221f16e91e21ef2053bc880d5864dcb
vinnietito <vincentnjuguna69@gmail.com>
2025-08-17 23:53:51 +0300

    Commits

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import React, { use, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import Home from './pages/Home/Home'
 import { Routes, Route, useNavigate } from 'react-router-dom'
 import Login from './pages/Login/Login'
 import Player from './pages/Player/Player'
 import { onAuthStateChanged } from 'firebase/auth'
 import { auth } from './firebase'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
@@ -24,6 +26,7 @@ useEffect(()=>{
 
   return (
     <div>
+      <ToastContainer theme='dark'/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/login' element={<Login/>}/>

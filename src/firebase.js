96a3594026e4bf7129a346cf5f34bf243a56f2bc
vinnietito <vincentnjuguna69@gmail.com>
2025-08-20 23:28:36 +0300

    Commits

--- src/firebase.js
+++ src/firebase.js
@@ -3,6 +3,7 @@ import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, si
 import { addDoc, collection, getFirestore } from "firebase/firestore";
 import { toast } from "react-toastify";
 
+// Load Firebase config from environment variables
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -12,38 +13,37 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-const signUp = async (name, email, password)=>{
-    try {
-        const res = await createUserWithEmailAndPassword(auth, email, password);
-        const user = res.user;
-        await addDoc(collection(db, "users"), {
-            uid: user.uid,
-            authProvider: "local",
-            email,
-        });
-    } catch (error) {
-        console.log(error);
-        toast.error(error.code.split('/')[1].split('-').join(" "));
-    }
-}
+const signUp = async (name, email, password) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+    await addDoc(collection(db, "users"), {
+      uid: user.uid,
+      authProvider: "local",
+      email,
+    });
+  } catch (error) {
+    console.log(error);
+    toast.error(error.code.split("/")[1].split("-").join(" "));
+  }
+};
 
-const login = async (email, password)=>{
-    try {
-        await signInWithEmailAndPassword(auth, email, password);
-    } catch (error) {
-        console.log(error)
-        toast.error(error.code.split('/')[1].split('-').join(" "));
-    }
-}
+const login = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (error) {
+    console.log(error);
+    toast.error(error.code);
+  }
+};
 
-const logout = ()=>{
-    signOut(auth);
-}
+const logout = () => {
+  signOut(auth);
+};
 
-export {auth, db, login, signUp, logout};
+export { auth, db, login, signUp, logout };
